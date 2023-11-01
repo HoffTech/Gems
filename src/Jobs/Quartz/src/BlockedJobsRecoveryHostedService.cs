3b90072e70fa6e1d2b9e9f7e72a0f5313e453156
Vedmid Andrey <Andrey.Vedmid@hofftech.ru>
2023-11-02 01:09:52 +0300

    [Gems.Jobs.Quartz] Добавить возможность восстановления заблокированных worker'ов - обработка ошибки и логирование в BackgroundService

--- src/Jobs/Quartz/src/BlockedJobsRecoveryHostedService.cs
+++ src/Jobs/Quartz/src/BlockedJobsRecoveryHostedService.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Gems.Jobs.Quartz.Configuration;
 
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 using Quartz;
@@ -20,10 +21,14 @@ namespace Gems.Jobs.Quartz
     public class BlockedJobsRecoveryHostedService : BackgroundService
     {
         private readonly IOptions<JobsOptions> options;
+        private readonly ILogger<BlockedJobsRecoveryHostedService> logger;
 
-        public BlockedJobsRecoveryHostedService(IOptions<JobsOptions> options)
+        public BlockedJobsRecoveryHostedService(
+            IOptions<JobsOptions> options,
+            ILogger<BlockedJobsRecoveryHostedService> logger)
         {
             this.options = options;
+            this.logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken cancellationToken)
@@ -40,43 +45,71 @@ namespace Gems.Jobs.Quartz
 
             while (true)
             {
-                var blockedJobsRecoveryOptions = this.options.Value.BlockedJobsRecovery;
-                var workersToRecover = blockedJobsRecoveryOptions.WorkersToRecover;
-                var maxDelayBetweenLastFireTimeAndRecoverTime = new TimeSpan(blockedJobsRecoveryOptions
-                    .MaxDelayBetweenLastFireTimeAndRecoverTimeInMilliseconds);
-
-                if (workersToRecover != null && workersToRecover.Count > 0)
+                try
                 {
-                    var triggerKeys = await scheduler
-                        .GetTriggerKeys(GroupMatcher<TriggerKey>.AnyGroup(), cancellationToken)
-                        .ConfigureAwait(false);
+                    var blockedJobsRecoveryOptions = this.options.Value.BlockedJobsRecovery;
+                    var workersToRecover = blockedJobsRecoveryOptions.WorkersToRecover;
+                    var maxDelayBetweenLastFireTimeAndRecoverTime = new TimeSpan(blockedJobsRecoveryOptions
+                        .MaxDelayBetweenLastFireTimeAndRecoverTimeInMilliseconds);
 
-                    foreach (var key in triggerKeys)
+                    if (workersToRecover != null && workersToRecover.Count > 0)
                     {
-                        var triggerState = await scheduler.GetTriggerState(key, cancellationToken).ConfigureAwait(false);
-                        if (triggerState is TriggerState.Blocked)
-                        {
-                            var trigger = await scheduler.GetTrigger(key, cancellationToken).ConfigureAwait(false);
-                            if (trigger != null && workersToRecover.Contains(trigger.JobKey.Name))
-                            {
-                                var lastFireTime = trigger.GetPreviousFireTimeUtc();
-                                var needToRecover = (DateTime.UtcNow - lastFireTime) > maxDelayBetweenLastFireTimeAndRecoverTime;
-                                if (needToRecover
-                                    && trigger is CronTriggerImpl cronTrigger
-                                    && !string.IsNullOrWhiteSpace(cronTrigger.CronExpressionString))
-                                {
-                                    var newTrigger = (CronTriggerImpl)cronTrigger.Clone();
-                                    newTrigger.CronExpression = new CronExpression(cronTrigger.CronExpressionString);
+                        var triggerKeys = await scheduler
+                            .GetTriggerKeys(GroupMatcher<TriggerKey>.AnyGroup(), cancellationToken)
+                            .ConfigureAwait(false);
 
-                                    await scheduler.UnscheduleJob(trigger.Key, cancellationToken).ConfigureAwait(false);
-                                    await scheduler.ScheduleJob(newTrigger, cancellationToken).ConfigureAwait(false);
+                        foreach (var key in triggerKeys)
+                        {
+                            var triggerState = await scheduler.GetTriggerState(key, cancellationToken)
+                                .ConfigureAwait(false);
+                            if (triggerState is TriggerState.Blocked)
+                            {
+                                var trigger = await scheduler.GetTrigger(key, cancellationToken).ConfigureAwait(false);
+                                if (trigger != null && workersToRecover.Contains(trigger.JobKey.Name))
+                                {
+                                    var lastFireTime = trigger.GetPreviousFireTimeUtc();
+                                    var needToRecover = (DateTime.UtcNow - lastFireTime) >
+                                                        maxDelayBetweenLastFireTimeAndRecoverTime;
+                                    this.logger.LogInformation("Trigger ({TriggerKey}) fired at ({TriggerLastFireTime}). Should it be recovered? ({TriggerShouldBeRecovered})", trigger.JobKey.Name, lastFireTime, needToRecover);
+                                    if (needToRecover
+                                        && trigger is CronTriggerImpl cronTrigger
+                                        && !string.IsNullOrWhiteSpace(cronTrigger.CronExpressionString))
+                                    {
+                                        var newTrigger = (CronTriggerImpl)cronTrigger.Clone();
+                                        newTrigger.CronExpression =
+                                            new CronExpression(cronTrigger.CronExpressionString);
+
+                                        var unscheduleResult = await scheduler.UnscheduleJob(trigger.Key, cancellationToken)
+                                            .ConfigureAwait(false);
+                                        if (unscheduleResult)
+                                        {
+                                            await scheduler.ScheduleJob(newTrigger, cancellationToken)
+                                                .ConfigureAwait(false);
+                                            this.logger.LogInformation("Trigger ({TriggerKey}) successfully rescheduled", trigger.JobKey.Name);
+                                        }
+                                        else
+                                        {
+                                            this.logger.LogInformation("Trigger ({TriggerKey}) failed to unschedule", trigger.JobKey.Name);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        this.logger.LogInformation("Trigger ({TriggerKey}) shouldn't be recovered", trigger.JobKey.Name);
+                                    }
                                 }
                             }
                         }
                     }
                 }
-
-                await Task.Delay(blockedJobsRecoveryOptions.CheckIntervalInMilliseconds, cancellationToken).ConfigureAwait(false);
+                catch (Exception e)
+                {
+                    this.logger.LogError(e, "Recovery of blocked jobs failed");
+                }
+                finally
+                {
+                    await Task.Delay(this.options.Value.BlockedJobsRecovery.CheckIntervalInMilliseconds, cancellationToken)
+                        .ConfigureAwait(false);
+                }
             }
         }
     }
