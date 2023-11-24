3575cad96fe0fd279571e1bcad9df1721f466c2e
Vlasenko Sergey <sergey.vlasenko@rentateam.ru>
2023-11-24 08:35:00 +0000

    [Gems.Metrics] Исправление сброса метрик при передаче метрик с лейблами

--- src/Metrics/Metrics/src/Contracts/MetricInfo.cs
+++ src/Metrics/Metrics/src/Contracts/MetricInfo.cs
@@ -1,9 +1,12 @@
 // Licensed to the Hoff Tech under one or more agreements.
 // The Hoff Tech licenses this file to you under the MIT license.
 
+using System;
+using System.Linq;
+
 namespace Gems.Metrics.Contracts
 {
-    public struct MetricInfo
+    public struct MetricInfo : IEquatable<MetricInfo>
     {
         /// <summary>
         /// Описание метрики.
@@ -24,5 +27,21 @@ namespace Gems.Metrics.Contracts
         /// Значения для установки меток.
         /// </summary>
         public string[] LabelValues { get; set; }
+
+        public bool Equals(MetricInfo other)
+        {
+            return this.Name.Equals(other.Name, StringComparison.Ordinal) &&
+                   this.LabelValues.SequenceEqual(other.LabelValues);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is MetricInfo other && this.Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Name, string.Join(',', this.LabelValues));
+        }
     }
 }
