6da58fa7efb1400291259e85adeda5ee47b1aee9
Yahin Rifat <Rifat.Yahin@hoff.ru>
2023-12-27 15:58:44 +0300

    [Jobs.Quartz] слил реализация в jobs/list

--- src/Jobs/Quartz/src/Handlers/GetSchedulerInfo/GetSchedulerInfoQueryHandler.cs
+++ src/Jobs/Quartz/src/Handlers/GetSchedulerInfo/GetSchedulerInfoQueryHandler.cs
@@ -5,40 +5,63 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-using AutoMapper;
-
 using Gems.Jobs.Quartz.Handlers.GetSchedulerInfo.Dto;
 using Gems.Jobs.Quartz.Handlers.Shared;
-using Gems.Jobs.Quartz.Handlers.Shared.JobsInfoProvider;
 using Gems.Mvc.GenericControllers;
 
 using MediatR;
 
+using Quartz;
+using Quartz.Impl.Matchers;
+
 namespace Gems.Jobs.Quartz.Handlers.GetSchedulerInfo;
 
-[Endpoint("jobs/scheduler", "GET", OperationGroup = "jobs", Summary = "Получить информацию о шедулере")]
+[Endpoint("jobs/list", "GET", OperationGroup = "jobs", Summary = "Получить список заданий с триггерами")]
 public class GetSchedulerInfoQueryHandler : IRequestHandler<GetSchedulerInfoQuery, SchedulerInfo>
 {
     private readonly SchedulerProvider schedulerProvider;
-    private readonly JobsInfoProvider jobsInfoProvider;
-    private readonly IMapper mapper;
 
-    public GetSchedulerInfoQueryHandler(SchedulerProvider schedulerProvider, JobsInfoProvider jobsInfoProvider, IMapper mapper)
+    public GetSchedulerInfoQueryHandler(SchedulerProvider schedulerProvider)
     {
         this.schedulerProvider = schedulerProvider;
-        this.jobsInfoProvider = jobsInfoProvider;
-        this.mapper = mapper;
     }
 
     public async Task<SchedulerInfo> Handle(GetSchedulerInfoQuery request, CancellationToken cancellationToken)
     {
         var scheduler = await this.schedulerProvider.GetSchedulerAsync(cancellationToken).ConfigureAwait(false);
+        var jobKeys = await scheduler.GetJobKeys(GroupMatcher<JobKey>.AnyGroup(), cancellationToken).ConfigureAwait(false);
+        var jobsInfo = new List<JobInfo>();
+        foreach (var jobKey in jobKeys)
+        {
+            var jobInfo = new JobInfo();
+            var jobTriggers = await scheduler.GetTriggersOfJob(jobKey, cancellationToken).ConfigureAwait(false);
+            var triggersInfo = new List<TriggerInfo>();
+            foreach (var jobTrigger in jobTriggers)
+            {
+                var triggerInfo = new TriggerInfo { Trigger = jobTrigger };
+                if (jobTrigger is ICronTrigger cronTrigger)
+                {
+                    triggerInfo.CronExpression = cronTrigger.CronExpressionString;
+                }
+
+                var triggerState = await scheduler
+                    .GetTriggerState(jobTrigger.Key, cancellationToken)
+                    .ConfigureAwait(false);
+                triggerInfo.TriggerState = triggerState.ToString();
+
+                triggersInfo.Add(triggerInfo);
+            }
+
+            jobInfo.JobKey = jobKey;
+            jobInfo.Triggers = triggersInfo;
+            jobsInfo.Add(jobInfo);
+        }
 
         return new SchedulerInfo
         {
             SchedulerName = scheduler.SchedulerName,
             SchedulerInstanceId = scheduler.SchedulerInstanceId,
-            EnqueuedJobList = this.mapper.Map<List<JobInfo>>(await this.jobsInfoProvider.GetJobsInfo(scheduler, cancellationToken).ConfigureAwait(false))
+            EnqueuedJobList = jobsInfo
         };
     }
 }
